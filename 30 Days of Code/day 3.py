a86c351471060b5f039161a3c53fdea0e3b317c5
Danzel Serrano <dezioxe@gmail.com>
2019-03-12 17:00:56 -0400

    30 Days of Code
    30 Days of Code python solution

---
+++ 30 Days of Code/day 3.py
@@ -0,0 +1,7 @@
+#!/bin/python3
+if __name__ == '__main__':
+    N = int(input())
+    if(N % 2 != 0 or (N % 2 == 0 and (N > 5 and N < 21))):
+        print("Weird")
+    else:
+        print("Not Weird")
